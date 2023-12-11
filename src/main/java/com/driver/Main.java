6923382094b0a0051bb69860d5eac3ba29cfb84c
mohdharis123 <hh680700@gmail.com>
2023-12-12 03:14:03 +0530

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static class A{
+        public String haris(){
+            return "invoke method from class a ";
+        }
+
+    }
+    public static class B extends A{
+
+        public String haris() {
+            return "method is overridingin extended class b";
+        }
+    }
+    public static void main (String args[]){
+        B obj = new B();
+        obj.haris();
+    }
 }
